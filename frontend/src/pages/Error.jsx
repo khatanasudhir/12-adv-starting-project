c86fe22d1ca81f6ff2b21cf7a5ad1844a817b41f
khatanasudhir <khatanasudhir786@gmail.com>
2024-03-27 17:42:25 +0530

    new file updated

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -4,15 +4,15 @@ import { useRouteError } from "react-router-dom";
 function ErrorPage() {
   const error = useRouteError();
 
-  let title = "An error occured!";
+  //let title = "An error occured!";
   let message = "Something went wrong!";
 
   if (error.status === 500) {
     message = JSON.parse(error.data).message;
   }
 
-  if (error.status == 404) {
-    title = "Not found!";
+  if (error.status === 404) {
+    //title = "Not found!";
     message = "Could not find resource or page.";
   }
   return (
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,22 +1,20 @@
+import { Suspense } from "react";
 import EventsList from "../components/EventsList";
-import { json, useLoaderData } from "react-router-dom";
+import { json, useLoaderData, defer, Await } from "react-router-dom";
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  //   if (data.isError) {
-  //     return <p>{data.message}</p>
-  //   }
-  const events = data.events;
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
-
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -31,6 +29,13 @@ export async function loader() {
       }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
